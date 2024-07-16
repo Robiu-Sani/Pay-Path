77e43eb3303653c082823d2c58388abf2c3a8937
Robiu-Sani <hafazrobiussani@gmail.com>
2024-07-16 07:50:16 +0600

    banner is done

--- src/Components/MainComponent/Home/Banner.jsx
+++ src/Components/MainComponent/Home/Banner.jsx
@@ -1,7 +1,34 @@
+import bannerPhone from "../../../image/bannerPhone.png";
 export default function Banner() {
   return (
-    <div className="bg-gradient-bg w-full min-h-screen flex items-center justify-center text-white">
-      <h1 className="text-4xl">Banner</h1>
+    <div className="bg-gradient-bg w-full min-h-screen flex justify-center items-center pt-10 px-2 pb-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-center gap-10 text-white container">
+        <div className="w-full">
+          <h1 className="text-gradient text-center text-4xl sm:text-6xl md:text-[70px] md:text-left font-bold ">
+            EMPOWER YOUR FINANCES
+          </h1>
+          <p className="text-gradient font-bold my-5 text-center md:text-left">
+            PayPath simplifies financial transactions with secure, real-time
+            payments. Manage your money effortlessly, from sending funds to
+            tracking expenses. Experience fast, reliable, and secure digital
+            payments with PayPath. Join us today!
+          </p>
+          <button className="button border border-[#00eeff80]">
+            <span className="absolute text-gradient">Get The Card</span>
+          </button>
+        </div>
+        <div className="w-full flex justify-between">
+          <span></span>
+          <div className="w-full flex justify-center items-center relative">
+            <div className="w-[300px] sm:w-[400px] h-[300px sm:h-[400px] rounded-full bg-gradient-bg"></div>
+            <img
+              src={bannerPhone}
+              className="max-w-[450px] bannerImageStyle absolute top-[50%]"
+              alt="banner iconic mobile phone"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
